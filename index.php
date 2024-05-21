26e47fd0c7c08de831323d02a2a1e81a644bd0f8
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-05-21 10:15:25 -0300

    HTML básico e customizações no CSS

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Gerenciador de tarefas</title>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Gerenciador de Tarefas</h1>
+        </div>
+        <div class="form">  
+            <form action="" method="get">
+                <label for="task_name">Tarefa</label>
+                <input type="text" name="task_name" placeholder="Nome da Tarefa">
+                <button type="submit">Cadastrar</button>
+            </form>      
+        </div>
+        <div class="separator">
+
+        </div>
+        <div class="list-tasks">
+            <ul>
+                <li>Tarefa 1</li>
+                <li>Tarefa 2</li>
+                <li>Tarefa 3</li>
+            </ul>
+        </div>
+        <div class="footer">
+            <p>Desenvolvido por Pedro Müller</p>
+        </div>
+    </div>
+</body>
+</html>
