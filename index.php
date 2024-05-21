4ace0f6d94b07b271382c25147cb8aea12627ef8
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-05-21 11:06:48 -0300

    Inicio das implementações de PHP

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['tasks']))
+{
+    $_SESSION['tasks'] = array(); 
+}
+
+if (isset($_GET['task_name']))
+{
+    array_push($_SESSION['tasks'], $_GET['task_name']);
+    unset($_GET['task_name']);
+}
+if (isset($_GET['clear']))
+{
+    unset($_SESSION['tasks']);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,11 +40,21 @@
 
         </div>
         <div class="list-tasks">
-            <ul>
-                <li>Tarefa 1</li>
-                <li>Tarefa 2</li>
-                <li>Tarefa 3</li>
-            </ul>
+            <?php
+                if(isset($_SESSION['tasks']))
+                {
+                    echo "<ul>";
+                    
+                    foreach($_SESSION['tasks'] as $key => $task){
+                        echo "<li>$task</li>";
+                    }
+                    echo "</ul>";
+                }
+            ?>
+            <form action="" method="get">
+                <input type="hidden" name="clear" value="clear">
+                <button type="submit">Limpar Tarefas</button>
+            </form>
         </div>
         <div class="footer">
             <p>Desenvolvido por Pedro Müller</p>
