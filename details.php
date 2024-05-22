49b8dcf629c6d747fed560d0a2ee8fb4cbed79f9
PedroAugustoMuller <pedro.muller.a@gmail.com>
2024-05-22 11:53:38 -0300

    CSS da Tarefa detalhada

--- details.php
+++ details.php
@@ -12,7 +12,8 @@ $data = $_SESSION['tasks'][$_GET['key']];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/style/details.css">
+    <link rel="stylesheet" href="/style/index.css">
     <title><?php echo "Tarefa: ".  $data['task_name'];?></title>
 </head>
 <body>
@@ -33,11 +34,11 @@ $data = $_SESSION['tasks'][$_GET['key']];
             <div class="image">
                 <img src="uploads/<?php echo $data['task_image']?>" alt="Imagem tarefa">
             </div>
-            <div class="footer">
-                <p>Desenvolvido por Pedro Müller</p>
-            </div>
+            
         </div>
     </div>
-    
+    <div class="footer">
+                <p>Desenvolvido por Pedro Müller</p>
+    </div>
 </body>
 </html>
